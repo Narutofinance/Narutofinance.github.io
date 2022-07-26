0606a8261b05bb46eeff4b406b22a1619d98c150
Narutofinance <109944018+Narutofinance@users.noreply.github.com>
2022-07-26 04:39:21 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+(function($) {
+    $(function() {
+        $("#upper").click(function() {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+    });
+});
